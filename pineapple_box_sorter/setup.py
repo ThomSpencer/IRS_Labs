babba849648eb32b32b748d8072d1b48f14ffa4d
ThomSpencer <thomwilliamspencer2@gmail.com>
2025-09-29 10:50:09 +0000

    Fixing Localization

--- pineapple_box_sorter/setup.py
+++ pineapple_box_sorter/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+from glob import glob
+import os
 
 package_name = 'pineapple_box_sorter'
 
@@ -10,6 +12,10 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*')),
+        ('share/' + package_name + '/config', glob('config/*.yaml')),
+        ('share/' + package_name + '/rviz', glob('rviz/*.rviz')),
+        ('share/' + package_name + '/map', glob('map/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -20,7 +26,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'box_sorter = pineapple_box_sorter.box_sorter:main'
+            'box_sorter = pineapple_box_sorter.box_sorter:main',
+            'hs_waypoint_follower = hand_solo_virtual_nav.hs_waypoint_follower:main'
         ],
     },
 )
